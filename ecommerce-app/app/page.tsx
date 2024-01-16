266dc4ec536afeca5b26375efbb121f91b10bf17
Andrey Frolov <senior.slender2015@gmail.com>
2024-01-16 20:09:01 +0200

    fakestoreapi

--- ecommerce-app/app/page.tsx
+++ ecommerce-app/app/page.tsx
@@ -1,9 +1,19 @@
-import Image from 'next/image'
+import Product from "@/components/Product";
 
-export default function Home() {
-  return (
-    <main className="">
-      <h1>Hello, world!</h1>
-    </main>
-  )
+export default async function Home() {
+    const res = await fetch("https://fakestoreapi.com/products");
+    const products: Product[] = await res.json()
+
+    // console.log(products);
+    
+    return (
+        <main className="">
+        <section className="flex flex-col space-y-12 pb-44">
+            <h1 className="text-5xl font-bold text-center">DEALS OF THE DAY</h1>
+            {products.map((product)=>(
+            <Product key={product.id} product={product} />
+            ))}
+        </section>
+        </main>
+    );
 }
